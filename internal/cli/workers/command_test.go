f0453b39bb9a9890f9805927f7002ea9319033e8
outway <igotathing@gmail.com>
2021-06-08 18:01:11 +0500

    Update command_test.go

--- internal/cli/workers/command_test.go
+++ internal/cli/workers/command_test.go
@@ -3,7 +3,7 @@ package workers_test
 import (
 	"testing"
 
-	"github.com/spiral/roadrunner-binary/v2/internal/cli/workers"
+	"github.com/outway/roadrunner-binary/v2/internal/cli/workers"
 
 	"github.com/spiral/roadrunner/v2/plugins/config"
 	"github.com/stretchr/testify/assert"
