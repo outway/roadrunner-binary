24748872966996a124008cbd195c2c87d997921d
outway <igotathing@gmail.com>
2021-06-08 18:01:46 +0500

    Update command_test.go

--- internal/cli/serve/command_test.go
+++ internal/cli/serve/command_test.go
@@ -3,7 +3,7 @@ package serve_test
 import (
 	"testing"
 
-	"github.com/spiral/roadrunner-binary/v2/internal/cli/serve"
+	"github.com/outway/roadrunner-binary/v2/internal/cli/serve"
 
 	"github.com/spiral/roadrunner/v2/plugins/config"
 	"github.com/stretchr/testify/assert"
